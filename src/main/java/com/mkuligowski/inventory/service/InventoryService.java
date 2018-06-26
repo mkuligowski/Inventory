0ae38e6d3cbf21712e0638705f3911aaf0dfda78
Mariusz Kuligowski <kulig15@gmail.com>
2018-06-26 23:06:55 +0200

    Remove getProducts(), polishing

--- src/main/java/com/mkuligowski/inventory/service/InventoryService.java
+++ src/main/java/com/mkuligowski/inventory/service/InventoryService.java
@@ -3,15 +3,10 @@ package com.mkuligowski.inventory.service;
 import com.mkuligowski.inventory.domain.Category;
 import com.mkuligowski.inventory.domain.Product;
 
-import java.util.Map;
-import java.util.Set;
-
 public interface InventoryService {
-
     Integer getTotalQuantity();
     Integer getProductsQuantityByCategory(Category category);
     Integer getProductQuantity(Product product);
-    Map<Product, Integer> getProducts();
     void addProduct(Product product, Integer quantity);
     void setProductQuantity(Product product, Integer quantity);
 }
