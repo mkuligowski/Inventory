e3608c1a4069808afb7dd29dfe577b3274ed1ffc
Mariusz Kuligowski <kulig15@gmail.com>
2018-10-30 09:29:59 +0100

    Add new methods for manipulating products

--- src/main/java/com/mkuligowski/inventory/service/InventoryService.java
+++ src/main/java/com/mkuligowski/inventory/service/InventoryService.java
@@ -3,10 +3,12 @@ package com.mkuligowski.inventory.service;
 import com.mkuligowski.inventory.domain.Category;
 import com.mkuligowski.inventory.domain.Product;
 
+import java.util.List;
+
 public interface InventoryService {
     Integer getTotalQuantity();
     Integer getProductsQuantityByCategory(Category category);
-    Integer getProductQuantity(Product product);
-    void addProduct(Product product, Integer quantity);
-    void setProductQuantity(Product product, Integer quantity);
+    Product getProduct(String code);
+    void addProduct(Product product);
+    List<Product> getExpiredProducts();
 }
